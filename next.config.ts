6768236e544b8125b882316bdeda73dd22c7bacd
Lukas Prochazka <prochazka.lu@gmail.com>
2025-05-27 11:46:36 +0200

    Remove image domain

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   experimental: {
     typedRoutes: true,
   },
-  images: {
-    domains: ['avatars.githubusercontent.com'],
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
